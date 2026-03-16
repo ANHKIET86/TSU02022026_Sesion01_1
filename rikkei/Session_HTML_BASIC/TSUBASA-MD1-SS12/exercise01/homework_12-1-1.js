e36a777d898c9482bf55473118a7479060aea491
Nguyen Anh Kiet <nguyenanhkiet@MacBook-Pro-cua-Nguyen.local>
2026-03-16 21:20:02 +0900

    update homework html

---
+++ rikkei/Session_HTML_BASIC/TSUBASA-MD1-SS12/exercise01/homework_12-1-1.js
@@ -0,0 +1,8 @@
+let a = Number(prompt("Nhập số a:"));
+let b = Number(prompt("Nhập số b:"));
+
+if (a % b === 0) {
+    console.log("a chia hết cho b");
+} else {
+    console.log("a không chia hết cho b");
+}

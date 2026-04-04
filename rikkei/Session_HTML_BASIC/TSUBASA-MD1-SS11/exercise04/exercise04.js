f3bc58d2a668c7f1761e2705f5aa3bd68a5d20e0
Nguyen Anh Kiet <nguyenanhkiet@MacBook-Pro-cua-Nguyen.local>
2026-04-05 00:13:01 +0900

    update homework html

--- rikkei/Session_HTML_BASIC/TSUBASA-MD1-SS11/exercise04/exercise04.js
+++ rikkei/Session_HTML_BASIC/TSUBASA-MD1-SS11/exercise04/exercise04.js
@@ -1,16 +1,3 @@
-const original = {
-  name: 'Bob',
-  age: 30,
-  address: {
-    city: 'TPHCM',
-  },
-};
-
-const copy = JSON.parse(JSON.stringify(original));
-
-original.address.city = 'TPHCM mới';
-
-copy.name = 'Kelvin';
-
-console.log(original);
-console.log(copy);
+for (let i = 1; i <= 5; i++) {
+  console.log(`The number is ${i}`);
+}

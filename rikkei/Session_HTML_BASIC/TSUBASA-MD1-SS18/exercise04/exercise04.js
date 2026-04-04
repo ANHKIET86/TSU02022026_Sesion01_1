96654ab0559d95e6f5b0e7c755945cf66b2c44ce
Nguyen Anh Kiet <nguyenanhkiet@MacBook-Pro-cua-Nguyen.local>
2026-04-04 23:47:48 +0900

    update homework html

--- rikkei/Session_HTML_BASIC/TSUBASA-MD1-SS18/exercise04/exercise04.js
+++ rikkei/Session_HTML_BASIC/TSUBASA-MD1-SS18/exercise04/exercise04.js
@@ -1,7 +1,28 @@
-let items = document.querySelectorAll('.item');
-items.forEach((el) => {
-  el.onmouseover = function () {
-    let bgColor = el.getAttribute('id'); // lấy dữ liệu ở thuộc tính id của thẻ
-    document.body.style.backgroundColor = bgColor;
-  };
+const keyboard = document.getElementById("keyboard");
+const display = document.getElementById("display");
+
+// Tạo A → Z
+let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+letters.split("").forEach(letter => {
+    let btn = document.createElement("button");
+    btn.textContent = letter;
+    btn.className = "key";
+
+    btn.onclick = () => {
+        display.value += letter;
+    };
+
+    keyboard.appendChild(btn);
 });
+
+// Nút xóa
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "Xóa";
+deleteBtn.className = "key delete";
+
+deleteBtn.onclick = () => {
+    display.value = display.value.slice(0, -1);
+};
+
+keyboard.appendChild(deleteBtn);

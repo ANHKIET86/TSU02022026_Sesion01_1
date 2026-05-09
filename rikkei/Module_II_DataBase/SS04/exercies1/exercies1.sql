2cfbb1048151c799d9f28756b1be7dbeaca136ed
Nguyen Anh Kiet <email_cua_ban@example.com>
2026-05-09 22:07:26 +0900

    add Module_II_DataBase

---
+++ rikkei/Module_II_DataBase/SS04/exercies1/exercies1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE students (
+    student_id VARCHAR(20) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    gender VARCHAR(10) CHECK (gender IN ('Male', 'Female')),
+    email VARCHAR(100) UNIQUE
+);
+
+INSERT INTO students (student_id, full_name, birth_date, gender, email) 
+VALUES
+('SV001', 'Nguyen Van A', '1999-01-01', 'Male', 'nguenvana@gmail.com'),
+('SV002', 'Nguyen Van B', '1996-06-29', 'Male', NULL),
+('SV003', 'Nguyen Thi C', '1995-12-23', 'Female', NULL),
+('SV004', 'Nguyen Thi D', '1999-05-06', 'Female', 'nguenthid@gmail.com'),
+('SV005', 'Nguyen Van E', '2000-04-01', 'Male', 'nguenvane@gmail.com');
+
+SELECT * FROM students;
+
+SELECT student_id, full_name, email FROM students;
